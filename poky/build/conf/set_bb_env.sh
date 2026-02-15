86405ccf58e1037072f2e8275cc9e3506ab9e970
Kerigan Creighton <kerigancreighton@gmail.com>
2026-02-14 19:52:00 -0600

    Re-add some stuff, add a basic new linux recipe

--- poky/build/conf/set_bb_env.sh
+++ poky/build/conf/set_bb_env.sh
@@ -219,7 +219,7 @@ function build-prod() {
 }
 
 # cleared every time
-cleanList=(victor wired vic-cloud core-image-anki-initramfs rampost anki-version machine-robot-image system-conf extra-conf vic-engine vic-robot update-os update-engine wireutils wlan-opensource wcnss mm-camera initscript-anki rebooter adreno adsprpc vic-anim vic-switchboard vic-gateway-cert base-files libpvictor fake-hwclock purplpkg wireos-recovery)
+cleanList=(victor wired vic-cloud core-image-anki-initramfs rampost anki-version machine-robot-image system-conf extra-conf vic-engine vic-robot update-os update-engine wireutils initscript-anki rebooter vic-anim vic-switchboard vic-gateway-cert base-files libpvictor fake-hwclock purplpkg wireos-recovery)
 
 function clean-oskr() {
   unset_bb_env
