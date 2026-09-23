2602d762d5e8bcc951bbd096a31d016384fdbf92
Kerigan Creighton <kerigancreighton@gmail.com>
2026-09-22 21:26:37 -0500

    fix temper script, fix PC

--- poky/victor/meta-anki/recipes-helpful/wireutils/wireutils/temper.sh
+++ poky/victor/meta-anki/recipes-helpful/wireutils/wireutils/temper.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 while true; do
-    temps=()
+    raw=()
     for i in {0..3}; do
-        temps+=( $(< /sys/class/thermal/thermal_zone$i/temp) )
+        raw+=( $(< /sys/class/thermal/thermal_zone$i/temp) )
     done
     sum=0
-    for t in "${temps[@]}"; do (( sum+=t )); done
-    avg=$(awk "BEGIN{printf(\"%.1f\", $sum/${#temps[@]})}")
+    temps=()
+    for t in "${raw[@]}"; do
+        (( sum+=t ))
+        temps+=( $(awk "BEGIN{printf(\"%.1f\", $t/1000)}") )
+    done
+    avg=$(awk "BEGIN{printf(\"%.1f\", $sum/${#raw[@]}/1000)}")
     printf "\rtemps: %s | avg: %s°C   " \
       "$(printf '%s°C ' "${temps[@]}")" "$avg"
     sleep 0.5
